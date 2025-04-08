6561c01ee573938855aa1d399be72587329b6d01
Declan-QA <Declan.Ababio-Bogyah@gmail.com>
2025-04-08 17:01:18 +0100

    Prep for sQL

--- Backend.js
+++ Backend.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { validateEmailDomain } from './functions.js';
+import { paswordcheck, validateDate, validateEmailDomain } from './functions.js';
 console.log("Please run index.html with:\n  Your Webbrowser\n  Live Server Extension \nNot: \n  Live preview")
 const app = express();
 
@@ -13,7 +13,6 @@ app.post('/upload', middle,function(request, response) {
 });
 
 app.post("/validateEmail",middle, function(request,response){
-    console.log("run functions to check this")
     let responsedata;
     if (validateEmailDomain(request.body.email)){
         responsedata = true
@@ -25,6 +24,27 @@ app.post("/validateEmail",middle, function(request,response){
     response.json({ "data": responsedata });
 });
 
+app.post("/validateDate",middle, function(request,response){
+    let responsedata;
+    if (validateDate(request.body.birthday)){
+        responsedata = true
+    } else {
+        responsedata = [
+            "<p class='error'>Birthday cannot be a future date</p>"
+        ]
+    }
+    response.json({ "data": responsedata });
+});
 
+app.post("/validatePassword",middle, function(request,response){
+    let responsedata;
+    let check = paswordcheck(request.body.password);
+    if ( check === true){
+        responsedata = true
+    } else {
+        responsedata = check.map(value =>{return `<p class='error'>${value}</p>`}).join("")
+    }
+    response.json({ "data": responsedata });
+});
 
 app.listen(3000);
--- Form.js
+++ Form.js
@@ -9,15 +9,25 @@ document.getElementById('add-skill').addEventListener('click', function() {
     }
 });
 
-
+emailnode = document.getElementById("email")
+birthdaynode = document.getElementById("birthday")
+passwordnode = document.getElementById("password")
 const form = document.getElementById('user-form') 
+
+
+
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     const skills = [];
     const skillItems = document.getElementById('skills-list').getElementsByTagName('li');
+    const collection = form.querySelectorAll(".error")
+    for (const el of collection){
+        el.remove();
+    }
+    
     for (let i = 0; i < skillItems.length; i++) {
         skills.push(skillItems[i].textContent);
-    }   
+    }
     const forminnfo = new FormData(form)
     let outputarr = []
     let skillsarr= []
@@ -33,14 +43,12 @@ form.addEventListener('submit', function(event) {
     } else{
         skillsarr = "No skills"
     }
-    emailnode = document.getElementById("email")
-    birthdaynode = document.getElementById("birthday")
-    passwordnode = document.getElementById("password")
+
     
     outputarr.push(["skills",skillsarr])
     const sendata = Object.fromEntries(outputarr)
         //  document.getElementById("email").insertAdjacentHTML("afterend","<p>Make sure email ends in a valid domain</p>")
-        
+    let passes = 0;
     fetch('http://localhost:3000/validateEmail', {
         method: "POST",
         headers: {
@@ -49,12 +57,14 @@ form.addEventListener('submit', function(event) {
         body: JSON.stringify({ email: sendata.email })
     }).then(response => response.json())
     .then(result => {
-        console.log(result)
         data = result.data
         length= data.length
         if (data === true){
-            console.log("Progress");            
+            console.log("rmail_clear")
+            passes +=1
+            console.log(passes)
         } else{
+            emailclear=false
             emailnode.insertAdjacentHTML("afterend",data)
         }
 
@@ -63,12 +73,56 @@ form.addEventListener('submit', function(event) {
         console.error('Error:', error);
     });
 
+    fetch('http://localhost:3000/validateDate', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ birthday: sendata.birthday })
+    }).then(response => response.json())
+    .then(result => {
 
+        data = result.data
+        length= data.length
+        if (data === true){
+            console.log("date_clear")
+            passes +=1
+            console.log(passes)
+        } else{
+            birthdayclear = false
+            birthdaynode.insertAdjacentHTML("afterend",data)
+        }
 
-    
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
-   
+    fetch('http://localhost:3000/validatePassword', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ password: sendata.password })
+    }).then(response => response.json())
+    .then(result => {
+        data = result.data
+        length= data.length
+        if (data === true){
+            console.log("pass_clear")
+            passes +=1
+            console.log(passes)
+        } else{
+            passwordnode.insertAdjacentHTML("afterend",data)
+        }
 
-    
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    if (passes ==3){
+        
+    }
 });
 
